db05582581b0318ade453acc29df9c6cbb04cd8d
Rajat <rajatsachan37@gmail.com>
2022-02-16 14:15:16 +0530

    Initial commit

--- app.js
+++ app.js
@@ -11,23 +11,20 @@ app.get("/", function (req, res) {
 
 app.get("/results", function (req, res) {
   var query = req.query.search;
-  request(
-    "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb",
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var data = JSON.parse(body);
-        res.render("results", { data: data });
-      } else {
-        res.send("Nothing found go back!");
-      }
+  request("<INSERT API KEY HERE>", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      res.render("results", { data: data });
+    } else {
+      res.send("Nothing found");
     }
-  );
+  });
 });
 
 app.get("*", function (req, res) {
-  res.send("Nothing Found Go Back!");
+  res.send("Nothing Found");
 });
 
-app.listen(process.env.PORT, process.env.IP, function () {
+app.listen(3000, process.env.IP, function () {
   console.log("Movie App has started!");
 });
